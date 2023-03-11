a7fe426a8b04345b2dcd7ffc00ad8b1043b9c661
Stephen Fong <fongchinghinunsw@gmail.com>
2023-03-11 19:12:29 +1100

    First commit

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+const typeDefs = `#graphql
+    type Book {
+        title: String
+        author: String
+    }
+
+    type Query {
+        books: [Book]
+    }
+`;
+
+const books = [{ title: "The Awakening", author: "Stephen Fong" }];
+
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
+
+console.log(`Server ready at: ${url}`);
