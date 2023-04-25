83eb993f3c13073cc427c347fda16f2e48a2034a
Stephen Fong <fongchinghinunsw@gmail.com>
2023-04-25 18:22:01 +1000

    Modify get user query response

--- src/graphql/resolver/user/query.ts
+++ src/graphql/resolver/user/query.ts
@@ -3,24 +3,6 @@ import logger from '../../../utils/logger';
 
 export const user = async (parent: any, { id }: { id: string }) => {
   logger.info({ id }, 'Getting user by id');
-  try {
-    const { userId, familyName, givenName, profile, createdAt, updatedAt } = await userService.getUserById(id);
-    return {
-      user: {
-        userId,
-        familyName,
-        givenName,
-        profile,
-        createdAt,
-        updatedAt,
-      },
-      message: `Found user ${id}`,
-      success: true,
-    };
-  } catch (err) {
-    return {
-      message: err.message,
-      success: false,
-    };
-  }
+  const user = await userService.getUserById(id);
+  return user;
 };
