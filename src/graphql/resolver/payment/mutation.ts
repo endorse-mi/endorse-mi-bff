4530a0bb24d83bdaec2427f2a37233dc4f246843
Stephen Fong <fongchinghinunsw@gmail.com>
2023-08-02 21:13:20 +1000

    Update cart Ids

--- src/graphql/resolver/payment/mutation.ts
+++ src/graphql/resolver/payment/mutation.ts
@@ -1,12 +1,17 @@
-import { STRIPE_KEY, WEB_APP_URL } from '../../../environments';
+import { ENVIRONMENT, STRIPE_KEY, WEB_APP_URL } from '../../../environments';
 
 const stripe = require('stripe')(STRIPE_KEY);
 
-export const PRICE_IDS = {
-  price_1NWE0QFCMh4P4xJl0iPbPLHf: 1000,
-  price_1NWE3nFCMh4P4xJlSu4y47Yi: 5000,
-  price_1NWE3nFCMh4P4xJlXHa9QgfX: 15000,
-};
+export const PRICE_IDS =
+  ENVIRONMENT === 'dev'
+    ? {
+        price_1NacXqFCMh4P4xJlr6csjlKq: 1000,
+        price_1NacZPFCMh4P4xJlCVeGp2PV: 5000,
+      }
+    : {
+        price_1NacbRFCMh4P4xJlHixML4s7: 1000,
+        price_1NacbRFCMh4P4xJlc2YSGysP: 5000,
+      };
 
 export const createCheckoutSession = async (parent, { priceId }: { priceId: string }, context) => {
   if (!(priceId in PRICE_IDS)) {
